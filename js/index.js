ec9b9e7a2df61c60c587dece2c2c263d23eb4e8a
Timothy A Breeding <tim.breeding@yahoo.com>
2018-05-11 11:04:21 +0200

    Done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-  // Code to be executed when the DOM is ready
+  $("li").click(function() {
+    if($(this).hasClass("unchecked")) {
+      $(this).toggleClass("unchecked");
+      $(this).find("img").attr("src", "/img/check.svg"); 
+    } else {
+      $(this).toggleClass("unchecked");
+      $(this).find("img").attr("src", "/img/uncheck.svg"); 
+    }
+  })
 });
+
+// $(document).ready(function() {
+//   // Code to be executed when the DOM is ready
+//   const listItems = $("li");
+  
+//   listItems.click((listItem) => {
+
+//     let item = $(listItem.currentTarget);
+
+//     if(item.hasClass("unchecked")) {
+//       item.toggleClass("unchecked");
+//       item.children("img")[0].src = "/img/check.svg"; 
+//     } else {
+//       item.toggleClass("unchecked");
+//       item.children("img")[0].src = "/img/uncheck.svg"; 
+//     }
+//   })
+// });
